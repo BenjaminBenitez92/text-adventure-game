562aa79afffccb675b6817f994c2ae7e6da330ac
BenjaminBenitez92 <81438491+BenjaminBenitez92@users.noreply.github.com>
2023-09-26 15:58:34 -0400

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 
+def main():
+
+    name = input("what is you name? ")
+    options = ['Light', 'Dark']
+    sides = ''
+    output_message = 'Which side of the force would you like to use? \n'
+    for index, item in enumerate(options):
+        output_message += f'{index+1}) {item}\n'
+
+    while sides.capitalize() not in options:
+        sides = input(output_message)
+    
+    if (sides == 'Dark'):
+        dark(name)
+
+    # else:
+    #     light(name)
+
+
+def dark(name):
+
+
+
+main()
